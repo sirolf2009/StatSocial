7f9d6c58b68aa592d37fe9dc9c7119f574749a24
Coen <coendenengelsman@gmail.com>
2014-03-17 17:42:26 +0100

    Moved function for getting ndw data to ndw model Added collumn to ndw data situation_id

--- StatSocial/application/controllers/Roaddata.php
+++ StatSocial/application/controllers/Roaddata.php
@@ -30,41 +30,9 @@ class Roaddata extends CI_Controller {
 	 * Get actual NDW records and store in db
 	 */
 	public function get() {
-		$this->load->driver('request');
-		$this->request->select_driver('ndw');
-		$result = $this->request->get();
-		$ndw    = array();
-		foreach ($result->situation as $situation) {
-			$ndwRec       = array();
-			$situationRec = $situation->situationRecord;
-
-			$location           = $situationRec->groupOfLocations->locationContainedInItinerary->location;
-			$ndwRec['location'] = null;
-			if (isset($location->alertCLinear->alertCMethod4PrimaryPointLocation->alertCLocation->specificLocation)) {
-				$ndwRec['location'] = (int)$location->alertCLinear->alertCMethod4PrimaryPointLocation->alertCLocation->specificLocation;
-			} else {
-				continue;
-			}
-
-			$ndwRec['latitude']  = (float)$location->locationForDisplay->latitude;
-			$ndwRec['longitude'] = (float)$location->locationForDisplay->longitude;
-
-			$cause                 = $situationRec->cause;
-			$ndwRec['type']        = (string)$cause->causeType;
-			$ndwRec['description'] = "";
-			if (isset($cause->causeDescription->values)) {
-				$ndwRec['description'] = (string)$cause->causeDescription->values[0]->value;
-			}
-
-			$ndwRec['date']       = strtotime((string)$situationRec->situationRecordCreationTime);
-			$ndwRec['start_date'] = strtotime((string)$situationRec->validity->validityTimeSpecification->overallStartTime);
-			$ndwRec['end_date']   = strtotime((string)$situationRec->validity->validityTimeSpecification->overallEndTime);
-
-			$ndw[] = $ndwRec;
-		}
-
-		$this->Ndw_model->insertBatch($ndw); //TODO check duplication
+		$this->Ndw_model->getActualData();
 	}
+
 }
 
 /* End of file roaddata.php */
