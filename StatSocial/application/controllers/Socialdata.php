c67c1d8327bae8d7a62a355cadb8ba99b4ce2efd
sirolf2009 <masterflappie@gmail.com>
2014-03-25 10:10:14 +0100

    Added search option to social data

--- StatSocial/application/controllers/Socialdata.php
+++ StatSocial/application/controllers/Socialdata.php
@@ -16,11 +16,19 @@ class Socialdata extends CI_Controller {
     
     public function index()
     {
-        $this->post_model->facebook("");
+        //$this->post_model->twitter("file A6");
         
         $this->load->view('layout/header', array('title' => 'Socialdata'));
         $this->load->view('layout/nav', array());
         $this->load->view('pages/socialdata', array());
+        $this->load->view('layout/footer', array());
+    }
+
+    public function search() {
+        $this->load->view('layout/header', array('title' => 'Socialdata'));
+        $this->load->view('layout/nav', array());
+        $result = $this->post_model->search($_POST["searchPlatform"], $_POST["searchPerson"], $_POST["searchValue"], 1);
+        $this->load->view('pages/socialdata', array("searchResult" => $result));
         $this->load->view('layout/footer', array());
     }
 }
--- StatSocial/application/views/pages/socialdata.php
+++ StatSocial/application/views/pages/socialdata.php
@@ -2,4 +2,55 @@
 <div class="container">
     <?php echo alerts(); ?>
     <?php echo alert("warning", "Helaas kon ik je nog niet voorzien van sociale data, zullen we het later nog eens proberen?", FALSE); ?>
+    <form action="/socialdata/search" role="form" method="post">
+   		<div class="row">
+   			<div class="col-md-2">
+    		<label for="searchValue">Platform?</label>
+    		<select id="searchPlatformInput" name="searchPlatform" class="form-control">
+  				<option>Twitter</option>
+  				<option>Facebook</option>
+  				<option>Allebij</option>
+			</select>
+			</div>
+			<div class="form-group col-md-2">
+    			<label for="searchPerson">Persoon?</label>
+    			<input id="searchPersonInput" name="searchPerson" type="text" class="form-control" placeholder="zoeken...">
+  			</div>
+			<div class="form-group col-md-8">
+    			<label for="searchValue">Waar zoek je naar?</label>
+    			<input id="searchValueInput" name="searchValue" type="text" class="form-control" placeholder="zoeken...">
+  			</div>
+		</div>
+		<input type="submit" class="btn btn-primary" value="Zoeken">
+    </form>
+    <br>
+	<div class="table-responsive">
+    		<?php if(isset($searchResult)) { 
+    			print("<input type='button' class='btn btn-info pull-left' value='vorige <'>");
+    			print("<input type='button' class='btn btn-info pull-right' value='volgende >'>");
+				print("<table class='table'>");
+    			foreach($searchResult as $data):?>
+					<tr>
+						<td>
+							<?=$data->type?>
+						</td>
+						<td>
+							<?=$data->name?>
+						</td>
+						<td>
+							<?=$data->message?>
+						</td>
+						<td>
+							<?=$data->location?>
+						</td>
+						<td>
+							<?=date('d-m-Y H:i', $data->date)?>
+						</td>
+					</tr>
+				<?php endforeach; 
+				print("</table>");
+    			print("<input type='button' class='btn btn-info pull-left' value='vorige <'>");
+    			print("<input type='button' class='btn btn-info pull-right' value='volgende >'>");
+			} ?>
+	</div>
 </div>
